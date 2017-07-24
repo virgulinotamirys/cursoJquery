88390ff6630171c5e2938eaf0161f94f89d7dfcb
Tamirys Virgulino <tamygemeas@gmail.com>
2017-07-24 11:21:30 -0300

    primeiro commit

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+// console.log("Olá mundo");
+ var frase = $(".frase").text();
+ var numPalavras = frase.split(" ").length;
+ var tamanhoFrase = $("#tamanho-frase");
+
+ tamanhoFrase.text(numPalavras);
+
+ var campo =  $(".campo-digitacao");
+campo.on("input", function(){
+	// console.log("cliquei");
+	var conteudo = campo.val();
+	var qtdPalavras = conteudo.split(/\S+/).length - 1;
+	$("#contador-palavras").text(qtdPalavras);
+	var qtdCaracteres = conteudo.length;
+	$("#contador-caracteres").text(qtdCaracteres);
+});
