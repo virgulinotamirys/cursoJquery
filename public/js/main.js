2c5c346089e62678d548427ccdde22ede7b48bb5
Tamirys Virgulino <tamygemeas@gmail.com>
2017-07-24 14:41:54 -0300

    game over com eventos

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,29 @@
 // console.log("Olá mundo");
- var frase = $(".frase").text();
- var numPalavras = frase.split(" ").length;
- var tamanhoFrase = $("#tamanho-frase");
+var frase = $(".frase").text();
+var numPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
 
- tamanhoFrase.text(numPalavras);
+tamanhoFrase.text(numPalavras);
 
- var campo =  $(".campo-digitacao");
-campo.on("input", function(){
+var campo =  $(".campo-digitacao");
+	campo.on("input", function(){
 	// console.log("cliquei");
 	var conteudo = campo.val();
 	var qtdPalavras = conteudo.split(/\S+/).length - 1;
 	$("#contador-palavras").text(qtdPalavras);
 	var qtdCaracteres = conteudo.length;
 	$("#contador-caracteres").text(qtdCaracteres);
+});
+
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function(){
+	var cronometroID = setInterval(function(){
+		tempoRestante--;
+		$("#tempo-digitacao").text(tempoRestante);
+		if(tempoRestante < 1){
+			campo.attr("disabled", true);
+			clearInterval(cronometroID);
+		}
+	},1000);
 });
