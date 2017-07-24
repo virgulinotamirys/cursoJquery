0bed005bdd2e8c9ec0ba8704a57f02952ed4292e
Tamirys Virgulino <tamygemeas@gmail.com>
2017-07-24 15:25:33 -0300

    estruturando o js

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,54 @@
-// console.log("Olá mundo");
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
-
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo =  $(".campo-digitacao");
+
+$(
+	function(){
+		atualizaTamanhoFrase();
+		inicializaContadores();
+		inicializaCronometro();
+		$("#botao-reiniciar").click(reiniciaJogo);
+	}
+);
+
+function atualizaTamanhoFrase(){
+	var frase = $(".frase").text();
+	var numPalavras = frase.split(" ").length;
+	var tamanhoFrase = $("#tamanho-frase");
+	tamanhoFrase.text(numPalavras);
+}
+
+
+function inicializaContadores() {
+
 	campo.on("input", function(){
-	// console.log("cliquei");
-	var conteudo = campo.val();
-	var qtdPalavras = conteudo.split(/\S+/).length - 1;
-	$("#contador-palavras").text(qtdPalavras);
-	var qtdCaracteres = conteudo.length;
-	$("#contador-caracteres").text(qtdCaracteres);
-});
+		var conteudo = campo.val();
+		var qtdPalavras = conteudo.split(/\S+/).length - 1;
+		$("#contador-palavras").text(qtdPalavras);
+		var qtdCaracteres = conteudo.length;
+		$("#contador-caracteres").text(qtdCaracteres);
+	});
+}
+	
 
+function inicializaCronometro() {
+	var tempoRestante = $("#tempo-digitacao").text();
+	campo.one("focus", function(){
+		var cronometroID = setInterval(function(){
+			tempoRestante--;
+			$("#tempo-digitacao").text(tempoRestante);
+			if(tempoRestante < 1){
+				campo.attr("disabled", true);
+				clearInterval(cronometroID);
+			}
+		},1000);
+	});
+}
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function(){
-	var cronometroID = setInterval(function(){
-		tempoRestante--;
-		$("#tempo-digitacao").text(tempoRestante);
-		if(tempoRestante < 1){
-			campo.attr("disabled", true);
-			clearInterval(cronometroID);
-		}
-	},1000);
-});
+function reiniciaJogo() {
+	campo.attr("disabled",false);
+	campo.val("");
+	$("#contador-palavras").text("0");
+	$("#contador-caracteres").text("0");
+	$("#tempo-digitacao").text(tempoInicial);
+	inicializaCronometro();
+}
