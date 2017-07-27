3b8b04db5c20f3aec58dd3e3fe8ea79120e8a6df
Tamirys Virgulino <tamygemeas@gmail.com>
2017-07-27 09:27:42 -0300

    add eventos nas linhas do placar

--- public/js/main.js
+++ public/js/main.js
@@ -80,15 +80,38 @@ function inserePlacar(){
 	var usuario = "Tamirys";
 	var numPalavras = $("#contador-palavras").text();
 
-	var linha = "<tr>"+
-                    "<td>"+ usuario + "</td>"+
-                    "<td>"+ numPalavras + "</td>"+
-                "</tr>";
+ 	var linha = novaLinha(usuario,numPalavras);
+ 	linha.find(".botao-remover").click(removeLinha);
 
     corpoTabela.prepend(linha);
 
 }
 
+function novaLinha(usuario,palavras){
+	var linha = $("<tr>");
+	var colunaUsuario = $("<td>").text(usuario);
+	var colunaPalavras = $("<td>").text(palavras);
+	var colunaRemover = $("<td>");
+	var link = $("<a>").addClass("botao-remover").attr("href", "#");
+	var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+	link.append(icone);
+
+	colunaRemover.append(link);
+
+	linha.append(colunaUsuario);
+	linha.append(colunaPalavras);
+	linha.append(colunaRemover);
+
+	return linha;
+
+}
+
+
+function removeLinha(){
+	event.preventDefault();
+    $(this).parent().parent().remove();
+}
 
 
 
